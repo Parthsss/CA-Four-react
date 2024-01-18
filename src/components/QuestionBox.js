226b3d11d841d58b78531fba893836b99d5e7ea2
Parthssh <naturallyinsane13@gmail.com>
2024-01-18 17:42:43 +0530

    basic layout

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import questions from "../questions";
+import Result from "./Result";
+// import "../styles/QuestionBox.css";
+import lightSun from "../assets/mode.png";
 
-export default function QuestionBox() {
+const QuestionBox = () => {
+  
+  const [darkMode, setDarkMode] = useState(false);
+  const [currentQuestion ] = useState(0) 
+  const [isHighlighted, setIsHighlighted] = useState(false);
+  const [showResult] = useState(false);
+
+ 
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+ 
+  function calculateScore() {
+  
+  }
+
+
   return (
-    <div>
-      
+    <div className={`main ${darkMode ? "dark" : "light"}`}>
+      <div className="top">
+        
+        <h1 >
+          React Quiz
+        </h1>
+        
+        <button onClick={toggleTheme} className="theme-btn">
+          { <img src={lightSun} />  }
+        </button>
+      </div>
+      <div className="container">
+        {!showResult ? (
+          
+          <div>
+            <div className="question-number">Question: {currentQuestion + 1} of {questions.length}</div>
+            <div  className="question">
+             
+              {questions[currentQuestion].text}
+            </div>
+           
+            
+            <div className="button-container">
+              <button
+                className={`highlight-button`}
+                onClick={() => setIsHighlighted(true)}
+              >
+                Highlight
+              </button>
+              <button
+                className={`remove-highlight-button`}
+                onClick={() => setIsHighlighted(false)}
+              >
+                Remove Highlight
+              </button>
+            </div>
+          </div>
+        ) : (
+         
+          <Result score={calculateScore()}  />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+
+export default QuestionBox;
